5ec2744b47e1c7e12ed8e43e863162bc1f241cbc
Dothivui03 <95094011+Dothivui03@users.noreply.github.com>
2021-12-04 19:57:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "I LOVE YOU",
-    introTitle: 'Phạm Văn Hóa',
+    titleWeb: "I LIKE YOU",
+    introTitle: 'Trần Đức Tập',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Sao tớ nhắn tin cậu không trả lời',
-    desc: 'Cậu dỗi tớ hỏ :( ',
-    btnYes: 'Tớ hong dỗi dou, vẫn cứ là yeucau <333',
-    btnNo: 'Tớ dỗi, ứ chơi với cậu nữa',
+    title: 'Từ lần đầu tiên khi kết bạn Zalo với cậu, tớ đã có một tình cảm đặc biệt dành cho cậu rồi',
+    desc: 'Bây giờ tớ muốn tỏ tình cậu.....làm người yêu tớ nhé <3 ',
+    btnYes: 'Tớ đồng ý <333',
+    btnNo: 'Không, tớ không thích cậu :)))',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Vì cậu là phiên bản cả Thế Giới chỉ có một',
+    reply: 'Vì cậu là phiên bản cả Thế Giới chỉ có một.....iu cậu nhiều lắm',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'ukiii, vậy mình tớ có làm sai điều gì thì cậu cho tớ xin lỗi nha.....đối với tớ cậu luôn đúng <3',
+    messDesc: 'ukiii, vậy xem xong thì ib qua messenger cho mình ngay nhé.....tớ chờ tin nhắn của cậu',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
