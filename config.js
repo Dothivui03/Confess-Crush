6a7d9c0d3dde5e8e0d8ecab7b8742062a654e3f1
Dothivui03 <95094011+Dothivui03@users.noreply.github.com>
2022-10-27 21:58:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Hi embe",
+    introTitle: 'Nguyễn Văn Phương',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Đi cháy phố với chị nhó🥰',
+    desc: 'em chỉ cần chở thui, còn tiền xăng để chị lo ',
+    btnYes: 'ok chị iuuu <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'embe có quý chị không nhờ <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'có. Vì chị là phiên bản cả Thế Giới chỉ có một <33333333',
+    mess: 'Mình biết mà 🥰. quý embe lắm 😘😘',
+    messDesc: 'lên lịch đi cháy phố thui nào.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
